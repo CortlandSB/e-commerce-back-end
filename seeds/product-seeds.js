c7b83f07cb224cc82766a42f6ac7412d64b05db8
Cortland Styles-Brown <cstylesbrown@gmail.com>
2021-09-07 08:46:25 -0500

    updated product-seeds

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,33 +2,33 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
-    price: 14.99,
-    stock: 14,
+    product_name: 'Grey T-Shirt',
+    price: 17.99,
+    stock: 23,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
-    price: 90.0,
-    stock: 25,
+    product_name: 'Hiking Boots',
+    price: 115.0,
+    stock: 18,
     category_id: 5,
   },
   {
-    product_name: 'Branded Baseball Hat',
-    price: 22.99,
-    stock: 12,
+    product_name: 'Winter Cap',
+    price: 36.99,
+    stock: 16,
     category_id: 4,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
-    price: 12.99,
-    stock: 50,
+    product_name: 'Stevie Wonder Greatest Hits Vinyl Record',
+    price: 15.99,
+    stock: 60,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
-    price: 29.99,
-    stock: 22,
+    product_name: 'Purple Shorts',
+    price: 30.00,
+    stock: 25,
     category_id: 2,
   },
 ];
