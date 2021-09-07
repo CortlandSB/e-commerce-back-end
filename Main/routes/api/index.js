f241afc931b8cf772fc05242d216e9d380d4b20f
Cortland Styles-Brown <cstylesbrown@gmail.com>
2021-09-07 01:01:06 -0500

    changed seeds file

--- Main/routes/api/index.js
+++ Main/routes/api/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
+const categoryRoutes = require('./category-routes');
 const tagRoutes = require('./tag-routes');
-
+router.use('/tags', tagRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
 
 module.exports = router;
